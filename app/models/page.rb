5825ed03ad39f84b7abb8510aa190e2a73ffcb8c
Eike Send <eike.send@gmail.com>
2011-10-04 17:29:45 +0200

    improved parent select menu

--- app/models/page.rb
+++ app/models/page.rb
@@ -6,4 +6,23 @@ class Page < ActiveRecord::Base
   scope :without_parent, :conditions => "parent_id is null" 
   scope :by_parent, lambda { |parent_id| {:conditions => ["parent_id = ?", parent_id]}}
 
+  def get_possible_parents()
+    unless section.pages.empty?
+      possible = section.pages.map do |p| 
+        if !p.nil? and p.id != id 
+          then [p.name, p.id] 
+        end
+      end 
+    end
+    possible.sort_by! do |e|
+      if e.is_a? Array
+        puts e[0], e[1]
+        e[0]
+      else
+        " "
+      end
+    end
+    possible
+  end
+
 end
