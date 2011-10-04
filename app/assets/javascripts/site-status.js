888579d626eca22eb3173dc9b2a2304f9b371270
Eike Send <eike.send@gmail.com>
2011-10-04 15:37:38 +0200

    some javascript fixes

--- app/assets/javascripts/site-status.js
+++ app/assets/javascripts/site-status.js
@@ -1,20 +1,21 @@
 $(function(){
   $('.page_status label').click(function(event){
-    console.log("click");
     $('.page_status label').show();
     $('.page_status select').hide();
     $(this).next().show().click();
     $(this).hide();
   });
-  $('.field input').bind("keypress, change", function(event){
-    $(this).parents("form").find(".actions input").show();
-  });
-/*
   $('.page_status select').change(function(event){
-    console.log("change");
     $(this).prev().show().removeAttr("class").addClass($(this).val());
     $(this).hide();    
-    $(this).parents("form").find(".pagesubmit input").show();
   });
-*/
+  
+  $('form').bind('ajax:error', function(event) {
+    alert("There was an error! Changes could not be saved.");
+  });
+  $('.delete_page').bind('ajax:success', function(event) {
+    $(this).parents("li").first().remove();
+  });
+
 });
+
